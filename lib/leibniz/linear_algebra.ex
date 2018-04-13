7fda9514b15f5d229eb2bccec4bcaae0851cdd25
Maddie <lusul101@gmail.com>
2018-04-13 17:46:39 -0600

    refactor linear algebra algorithm

--- lib/leibniz/linear_algebra.ex
+++ lib/leibniz/linear_algebra.ex
@@ -10,54 +10,35 @@ defmodule Leibniz.Linear do
   - `left` - a list containing expression structs that represent the
     expression on the right side of =
 
-  ## Examplea
+  ## Examples
 
-      # x + 3 = 5
+      # 4x = 16
       iex> Leibniz.Linear.solve(Leibniz.Expr{[{4, "x", 1}]}, [{16, "x", 0}])
       4
   """
   def solve(right, left, var) when is_binary(var) do
-      left = regroup(left)
-      right = regroup(right)
+      left = regroup(left, var)
+      right = regroup(right, var)
   end
 
   def regroup(expr, var) do
-    degree = expr_degree(expr)
-    do_regroup(expr, %Leibniz.Expr{}, degree, var)
+    similar_terms = Enum.group_by(expr.terms, &exponent(&1))
+
+    for {exponent, terms} <- similar_terms do
+      reduce(terms, var, exponent)
+    end
   end
 
-  defp do_regroup(_, new_expr, current_exponent, _) when current_exponent < 0 do
-    new_expr
-  end
-
-  defp do_regroup(expr, new_expr, current_exponent, var) do
-    sum = sum_of_simlilar_term(expr, current_exponent)
-    new_expr = %Leibniz.Linear{terms: [{sum, var, current_exponent} | new_expr.terms]}
-    do_regroup(expr, new_expr, current_exponent - 1, var)
-  end
-
-  defp sum_of_simlilar_terms(expr, current_exponent) do
-    expr = Enum.filter(expr.terms, fn(term) ->
-             {_, _, exponent} = term
-             exponent == current_exponent
-           end)
-
-    coefficients = for term <- expr.terms, exponent?(term, current_exponent) do
-                     {coefficient, _, _} = term
-                     coefficient
+  def reduce(similar_terms, var, exponent) do
+    coefficients = for {coefficiemt, _, _} <- similar_terms do
+                     coefficiemt
                    end
 
-    Enum.sum(coefficients)
+    {Enum.sum(coefficients), var, exponent}
   end
 
-  def exponent?(term, exponent) do
-
+  def exponent ({_, _, exponent}) do
+    exponent
   end
 
-  def expr_degree(expr) do
-    exponents = for {_, _, exponent} <- expr.terms do
-                  exponent
-                end
-    Enum.max(exponents)
-  end
 end
